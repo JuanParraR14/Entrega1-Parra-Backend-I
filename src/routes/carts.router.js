f71d07b5e85391f8e37552dcb878e8dc56414a40
JuanParra14 <juanparrar14@gmail.com>
2026-01-15 12:54:15 -0500

    Se crea la lógica entre el cart Manager y el cart router

---
+++ src/routes/carts.router.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import CartManager from "../managers/CartManager";
+
+const router = Router();
+const cartManager = new CartManager("./src/data/carts");
+
+router.post("/", async (req, res) => {
+   const cart = await cartManager.createCart();
+   res.status(201).json(cart);
+});
+
+router.get("/:cid", async (req, res) => {
+   const cart = await cartManager.getCartById(req.params.cid);
+   if (!cart) return res.status(404).json({ error: "Carrito no encontrado" });
+   res.json(cart.products);
+});
+
+router.post("/:cid/product/:pid", async (req, res) => {
+   const cart = await cartManager.addProductToCart(
+      req.params.cid,
+      req.params.pid
+   );
+
+   if(!cart) return res.status(404).json({ error: "Carrito no encontrado" });
+   res.json(cart);
+});
+
+export default router;
