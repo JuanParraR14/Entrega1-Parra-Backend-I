737500d67f7d15a23655cf62465602431e969bbd
JuanParra14 <juanparrar14@gmail.com>
2026-03-15 16:20:09 -0500

    Se rehace el archivo products.router para incluir los requisitos de la entrega acutal

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,43 +1,128 @@
 import { Router } from "express";
 import { io } from "../app.js";
-import ProductManager from "../managers/ProductManager.js";
+import Product from "../models/Product.js";
 
 const router = Router();
-const productManager = new ProductManager("./src/data/products.json");
 
 router.get("/", async (req, res) => {
-   const products = await productManager.getProducts();
-   res.json(products);
+   try {
+      const { limit = 10, page = 1, sort, query } = req.query;
+
+      const options = {
+         limit: parseInt(limit),
+         page: parseInt(page),
+         lean: true
+      };
+
+      if (sort) {
+         options.sort = { price: sort === "asc" ? 1 : -1};
+      }
+
+      let filter = {};
+
+      if (query) {
+         const [field, value] = query.split(":");
+
+         if (field === "status") {
+            filter[field] = value === "true";
+         } else {
+            filter [field] = value;
+         }
+      }
+
+      const result = await Product.paginate(filter, options);
+
+      const response = {
+         status: "success",
+         payload: result.docs,
+         totalPages: result.totalPages,
+         prevPage: result.prevPage,
+         nextPage: result.nextPage,
+         page: result.page,
+         hasPrevPage: result.hasPrevPage,
+         hasNextPage: result.hasNextPage,
+         prevLink: result.hasPrevPage
+            ? `/api/products?page=${result.prevPage}`
+            : null,
+         nextLink : result.hasNextPage
+            ? `/api/products?page=${result.nextPage}`
+            : null   
+      };
+
+      res.json(response);
+   } catch (error) {
+      res.status(500).json({
+         status: "error",
+         error: error.message
+      });
+   }
 });
 
 router.get("/:pid", async (req, res) => {
-   const product = await productManager.getProductById(req.params.pid);
-   if (!product) return res.status(404).json({ error: "Producto no encontrado" });
-   res.json(product);
+   try {
+
+      const product = await Product.findById(req.params.pid).lean();
+
+      if (!product) {
+         return res.status(404).json({ error: "Producto no encontrado" });
+      }
+
+      res.json(product);
+
+   } catch (error) {
+      res.status(500).json({ error: error.message });
+   }
 });
 
 router.post("/", async (req, res) => {
-   const newProduct = await productManager.addProduct(req.body);
-   
-   const products = await productManager.getProducts();
-   io.emit("updateProducts", products);
+   try {
+      const newProduct = await Product.create(req.body);
 
-   res.status(201).json(newProduct);
+      const products = await Product.find().lean();
+      io.emit("updateProducts", products);
+
+      res.status(201).json(newProduct);
+
+   } catch (error) {
+      res.status(500).json({ error: error.message });
+   }
 });
 
 router.put("/:pid", async (req, res) => {
-   const updated = await productManager.updateProduct(req.params.pid, req.body);
-   if (!updated) return res.status(404).json({ error: "Producto no encontrado" });
-   res.json(updated);
+   try {
+
+      const updated = await Product.findByIdAndUpdate(
+         req.params.pid,
+         req.body,
+         { new: true }
+      );
+
+      if (!updated) {
+         return res.status(404).json({ error: "Producto no encontrado" });
+      }
+
+      res.json(updated);
+
+   }  catch (error) {
+      res.status(500).json({ error: error.message });
+   }
 });
 
 router.delete("/:pid", async (req, res) => {
-   await productManager.deleteProduct(req.params.pid);
+   try {
 
-   const products = await productManager.getProducts();
-   io.emit("updateProducts", products);
+      const deleted = await Product.findByIdAndDelete(req.params.pid);
 
-   res.json({ message: "Producto eliminado" });
+      if (!deleted) {
+         return res.status(404).json({ error: "Producto no encontrado" });
+      }
+
+      const products = await Product.find().lean();
+      io.emit("updateProducts", products);
+
+   } catch (error) {
+      res.status(500).json({ error: error.message });
+   }
 });
 
 export default router;
